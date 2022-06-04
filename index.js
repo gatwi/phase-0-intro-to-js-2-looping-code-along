bf28dbefd427af020ab54037f9df176c4d0352fa
gatwi <faith.murithi@student.moringaschool.com>
2022-06-04 18:27:19 +0300

    commit to master

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(giftsArray, message){
+    let wonderfulMessage = []
+    for(let i = 0; i<giftsArray.length;i++){
+        wonderfulMessage.push(`Thank you, ${giftsArray[i]}, for the wonderful ${message} gift!`);
+    }
+
+    return wonderfulMessage;
+}
+
+function countDown(int){
+    let count = int
+  console.log(count)
+  while(count > 0){
+        count--
+      console.log(count)
+  }
+}
